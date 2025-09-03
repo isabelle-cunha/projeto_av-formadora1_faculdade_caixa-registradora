04684433a9df93c682b30d9d43f43ad73d156b0b
Isabelle Cunha <isabellabanus@gotmail.com>
2025-09-03 15:09:10 -0300

    Parte 2: cálculos base, total bruto e forma de pagamento.

--- av_formadora1.py
+++ av_formadora1.py
@@ -1,8 +1,8 @@
 #perguntar quantos produtos serão registrados [x]
 #para cada produto,solicitar: nome do produto, preço unitario e quantidade comprada. [x]
-#calcular o subtotal de cada produto (preço x quantidade)
-#somar ao total geral da compra
-#peguntar a forma de pagamento: 1 (a vista), 2(cartão de credito)
+#calcular o subtotal de cada produto (preço x quantidade)[x]
+#somar ao total geral da compra [x]
+#peguntar a forma de pagamento: 1 (a vista), 2(cartão de credito)[x]
 
 #aplicar descontos no total geral conforme: 
 #total acima de 200 reais a vista (15% de desconto)
@@ -16,41 +16,69 @@
 #valor total final a pagar
 
 #requisitos mínimos:
-#usar laço de repetição para registrar os produtos
-#uso de variáveis para controle do total e desconto
-#uso de estrutura condicional para aplicação das regras
-#saída formatada e clara.
+#usar laço de repetição para registrar os produtos[x]
+#uso de variáveis para controle do total e desconto[x]
+#uso de estrutura condicional para aplicação das regras[x]
+#saída formatada e clara.[]
 
 #entrada de dados
 qntd_produtos_informados=int(input("Informe, com números, quantos produtos serão registrados: "))
-forma_pagamento=int(input("Qual a forma de pagamento? Digite 1 para pagamento à vista e 2 para cartão de crédito: "))
+
 lista_produtos=[]
 valores_produtos=[]
 qntd_produtos=[]
+subtotal_cada=[]
+
 
 #processamento  
 def registrar_produto(qntd_produtos_informados):
     for n_produtos in range(qntd_produtos_informados):
-        nome_produto=input(f"Digite o nome do produto {(n_produtos)}: ")
+        nome_produto=input(f"Digite o nome do produto {n_produtos} que deseja registrar: ")
         lista_produtos.append(nome_produto)
     return lista_produtos
        
 def registrar_valor_unidade(qntd_produtos_informados):
     for i in range(qntd_produtos_informados):
-        valor_unidade=float(input(f"Digite o valor de {lista_produtos[i]}: "))
+        valor_unidade=float(input(f"Digite o valor da unidade de {lista_produtos[i]}: "))
         valores_produtos.append(valor_unidade)
     return valores_produtos
     
 def registrar_qntd_cada(qntd_produtos_informados):
-     for n_qntd in range(qntd_produtos_informados):
-        qntd_cada = int(input(f"Digite a quantidade de cada {lista_produtos [n_qntd]}: "))
+     for i in range(qntd_produtos_informados):
+        qntd_cada = int(input(f"Digite a quantidade de unidades de {lista_produtos [i]}: "))
         qntd_produtos.append(qntd_cada)
      return qntd_produtos
 
+def calcular_subtotal():
+    for i in range(len(qntd_produtos)):
+        subtotal= valores_produtos[i] * qntd_produtos[i]
+        subtotal_cada.append(subtotal)
+    return subtotal_cada
 
-       
-  
+def registrar_subtotal(subtotal_cada):
+    total_bruto=float(0)
+    for i in range(len(subtotal_cada)):
+        total_bruto = total_bruto + (subtotal_cada[i])
+        #python f'{var:.2f}'= javascript var.toFixed(2)
+    return f'{total_bruto:.2f}'
+
+#Definindo forma de pagamento:
+while True:
+    forma_pagamento=int(input("Qual a forma de pagamento? Digite 1 para pagamento à vista e 2 para cartão de crédito: "))
+    if forma_pagamento == 1:
+        pagamento='à vista'
+        print('Forma de pagamento:' ,pagamento)
+        break
+    elif forma_pagamento == 2:
+        pagamento='cartão de crédito'
+        print('Forma de pagamento:' ,pagamento)
+        break
+    else:
+        print("Forma de pagamento inválida.")
+
 #saída de dados
-print(registrar_produto(qntd_produtos_informados))
-print(registrar_valor_unidade(qntd_produtos_informados))
-print(registrar_qntd_cada(qntd_produtos_informados))
+print("Os produtos registrados foram:" ,registrar_produto(qntd_produtos_informados))
+print("O valor de cada unidade dos produtos registrados é:" ,registrar_valor_unidade(qntd_produtos_informados))
+print("A quantidade de unidades por produto é:" ,registrar_qntd_cada(qntd_produtos_informados))
+print("O subtotal de cada produto (quantidade de unidades x valor unidade) é:" ,calcular_subtotal())
+print("O valor total bruto da compra é: R$",registrar_subtotal(subtotal_cada))
